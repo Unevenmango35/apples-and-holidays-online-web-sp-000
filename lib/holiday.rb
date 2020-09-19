0e765079fa41ece17280479bff89a4f032eeb331
Rosie Choi <rosiechoimanso@gmail.com>
2020-09-19 04:25:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 holiday_hash.each do |key, value|
   value.each do |season, supply|
-    if season == 
+    if season == :christmas || :new_years
+      
 end
 
 
